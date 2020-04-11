30e50de8fad1925564856e82de7d1dfe39d74bce
zach <zachskellogg@gmail.com>
2020-04-11 12:37:21 -0600

    first big push

--- script.js
+++ script.js
@@ -98,22 +98,49 @@
 
 
 
-  
-function searchMovies(movie) {
 
-    var queryURL = "https://www.omdbapi.com/?s=random&apikey=d9a4745e";
-    var posterURL = "http://img.omdbapi.com/?t=" + movie + "&h=600&apikey=d9a4745e";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function searchMovies(movie) {
+    
+    for(var i = 0; i<50; i++){
+        pageNu = (Math.floor(Math.random() * 50));
+    }  
+
+    var queryURL = "https://www.omdbapi.com/?s=horror&page=" + pageNu + "&apikey=d9a4745e";
+    console.log(queryURL);
+    
+   
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+        for(var i = 0; i<50; i++){
+            arraySearch = (Math.floor(Math.random() * 10));
+        }  
+        console.log(arraySearch);
      
       console.log(response);
 
-      var movieName = $("<h1>").text(response.Title);
-      var movieImage = $("<img>").attr("src", response.Poster);
-        console.log(movieName);
-        console.log(movieImage);
+      var movieName = $("<h1>").text(response.Search[arraySearch].Title);
+      var movieImage = $("<img>").attr("src", response.Search[arraySearch].Poster);
+        
     
 
       $("#movieDiv").empty();
@@ -126,24 +153,6 @@ function searchMovies(movie) {
 
     searchMovies(movieSearch);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
