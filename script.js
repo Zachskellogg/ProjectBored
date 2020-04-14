24cdf63fad3f7ebaa96f151334bcb3fbdde3cda1
zach <zachskellogg@gmail.com>
2020-04-13 20:08:29 -0600

    cleaning up the branch

--- script.js
+++ script.js
@@ -117,11 +117,42 @@
 
 
 
-function searchMovies(movie) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function searchMovies() {
     
-    for(var i = 0; i<50; i++){
-        pageNu = (Math.floor(Math.random() * 50));
-    }  
+        pageNu = (Math.floor(Math.random() * 50) + 1);
+     
 
     var queryURL = "https://www.omdbapi.com/?s=horror&page=" + pageNu + "&apikey=d9a4745e";
     console.log(queryURL);
@@ -131,49 +162,55 @@ function searchMovies(movie) {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        for(var i = 0; i<50; i++){
-            arraySearch = (Math.floor(Math.random() * 10));
-        }  
-        console.log(arraySearch);
+        arraySearch = (Math.floor(Math.random() * 10));
+      
+      console.log(arraySearch);
      
       console.log(response);
+      if (response.Search[arraySearch].Poster === 'N/A') {
+        searchMovies();
+      }
 
       var movieName = $("<h1>").text(response.Search[arraySearch].Title);
       var movieImage = $("<img>").attr("src", response.Search[arraySearch].Poster);
         
-    
-
-      $("#movieDiv").empty();
-      $("#movieDiv").append(movieName, movieImage);
+      // $("#movieDiv").empty();
+      // $("#movieDiv").append(movieName, movieImage);
+      for ( i=0; i<6; i++) {
+      var newImg = `<div class=col-md-4 col-sm-6>
+      <div class=portfolio-item>
+          <h1> ${response.Search[arraySearch].Title}</h1>
+          <div class=image>
+              <img src=${response.Search[arraySearch].Poster}>
+          </div>
+      </div>
+    </div>`
+      
+    $('#movieDiv').append(newImg);
+      }
     });
   }
+
+  
   $("#searchMovie").on("click", function(event) {
     event.preventDefault();
     var movieSearch = $('#movieInput').val().trim();
 
     searchMovies(movieSearch);
-});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      });
+      
+      // <div class=hover-effect>
+      //     <div class=hover-content>
+      //         <h1>${albumName}</h1></a>
+      //         <p id=clickInfo${albumCount}>click track to listen to sample audio</p>
+      //         <div class=tracks id=album${albumCount}>
+      //         </div>
+      //     </div>
+      // </div>
+// Poster
+// name
+// Rating 
+// year 
+// plot 
 
